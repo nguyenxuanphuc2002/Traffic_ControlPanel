68d08fadd8cbf9f547164b91ca68c865afad15e4
phuc2305 <81752065+nguyenxuanphuc2002@users.noreply.github.com>
2023-01-12 16:17:54 +0700

    change ui

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from login import Ui_Login
+from ui import Ui_App
 from dashboardUI import UI_Dashboard
 import os
 import sys
@@ -13,70 +13,30 @@ from firebase_admin import firestore
 cred = credentials.Certificate("firebaseAccount.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
-global mail
-mail = "admin@gmail.com"
+
 class MainApplication(QtWidgets.QMainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
-        self.ui = UI_Dashboard()
+        self.ui = Ui_App()
         self.ui.setupUi(self)
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-        self.Login_App = Login()
-        self.Login_App.show()
-        self.ui.CloseBtn.clicked.connect(self.close)
-        self.ui.MinimunBtn.clicked.connect(self.showMinimized)
-        self.ui.logoutButton.clicked.connect(lambda: self.Logout())
-    def Logout(self):
-        w.close()
-        self.Login_App.show()
-class Dashboard(QtWidgets.QMainWindow):
-    def __init__(self):
-        QtWidgets.QMainWindow.__init__(self)
-        # self.Mail = mail
-        self.ui = UI_Dashboard()
-        self.ui.setupUi(self)
         self.show()
-        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
-        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-        # self.ui.logoutButton.clicked.connect(self.Logout)
-        self.ui.CloseBtn.clicked.connect(self.close)
-        self.ui.MinimunBtn.clicked.connect(self.showMinimized)
-        # name_acc = db.collection('account').document(mail).get().get("name")
-        # self.ui.nameInfor.setText(name_acc)
-    def Logout(self):
-        self.close()
-        # login_app.show()
-
-        
-
-
-        # self.openDB()
-    # def openDB(self):
-    #     self.db=QtSql.QSqlDatabase.addDatabase("QSQLITE")
-    #     self.db.setDatabaseName("accLogin.sqlite")
-    #     if not self.db.open():
-    #         print("error")
-    #     self.query = QtSql.QSqlQuery()
-      
-class Login(QtWidgets.QMainWindow):
-    def __init__(self):
-        QtWidgets.QMainWindow.__init__(self)
-        self.ui = Ui_Login()
-        self.ui.setupUi(self)
-        self.show()
-        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
-        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        # self.Login_App = Login()
+        # self.Login_App.show()
         self.ui.pushButton.clicked.connect(lambda : self.functionLogin())
-        self.ui.Showpassword.clicked.connect(self.showPass)
-        self.ui.Hidepassword.clicked.connect(self.hidePass)
+        self.ui.CloseBtn.clicked.connect(self.close)
+        self.ui.MinimunBtn.clicked.connect(self.showMinimized)
+        self.ui.logoutButton.clicked.connect(lambda: self.functionLogout())
     def functionLogin(self):
+        global mail
         Email=''
         page = auth.list_users()
         while page:
             for user in page.users:
                 if(self.ui.Email.text() == user.email):
                     #print(user.email)
+                    mail = Email
                     Email = user.email
                     break
             page=page.get_next_page()
@@ -86,8 +46,10 @@ class Login(QtWidgets.QMainWindow):
         else:
             passwords = db.collection('account').document(Email).get().get("pass")
             if(passwords == self.ui.password.text()):
-                self.close()
-                w.show()
+                self.ui.widget.hide()
+                self.ui.dashboard.show()
+                name = db.collection('account').document(Email).get().get("name")
+                self.ui.nameInfor.setText(name)
                 print("login successful")
     def showPass(self):
         self.ui.Hidepassword.show()
@@ -97,10 +59,23 @@ class Login(QtWidgets.QMainWindow):
         self.ui.Hidepassword.hide()
         self.ui.Showpassword.show()
         self.ui.password.setEchoMode(QtWidgets.QLineEdit.Password)
+    def functionLogout(self):
+        self.ui.dashboard.close()
+        self.ui.widget.show()
+
+
+#         # self.openDB()
+#     # def openDB(self):
+#     #     self.db=QtSql.QSqlDatabase.addDatabase("QSQLITE")
+#     #     self.db.setDatabaseName("accLogin.sqlite")
+#     #     if not self.db.open():
+#     #         print("error")
+#     #     self.query = QtSql.QSqlQuery()
+      
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    # dashboard = Dashboard()
-    # login_app = Login()
+    
     w = MainApplication()
     sys.exit(app.exec_())
 
